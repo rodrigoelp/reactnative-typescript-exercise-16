9131ad262e370c6e3b26230301eb17c5c0160f70
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-12-29 14:32:28 +1100

    Added type definition and exposed it in a very simple app component before I start migrating code from the previous sqlite implementation onto this one.

--- src/appshell.tsx
+++ src/appshell.tsx
@@ -1,7 +1,37 @@
 import * as React from "react";
 import { Text, View } from "react-native";
+import PouchDB from "pouchdb-react-native";
+
+interface Entity {
+    name: string;
+}
 
 class AppShell extends React.Component {
+    componentDidMount() {
+        console.log("componentDidMount");
+        const db = new PouchDB<Entity>("sample1");
+        db.put({
+                _id: "some random stuff",
+                name: "name"
+            })
+            .then(res => {
+                console.debug("Trying to attach to this.");
+            })
+            .catch(res => {
+                console.warn("Something bad just happened...");
+            });
+
+        setTimeout(() => {
+            db.get("some random stuff")
+                .then(r => {
+                    console.log("point to check what is going on here.");
+                })
+                .catch(res => {
+                    console.warn("failed to fetch the information from the database.");
+                });
+        }, 10000);
+    }
+
     public render() {
         return (
             <View style={{ flex: 1 }}>
